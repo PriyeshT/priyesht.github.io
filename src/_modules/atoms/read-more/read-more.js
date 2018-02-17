fb9ad26b7c0af1ea7ee95a07236134a41c9faa52
Priyesh Tungare <priyeshtungare@Priyeshs-MacBook-Pro.local>
2018-02-18 01:12:46 +0800

    added portfolio item styling and portfolio section

---
+++ src/_modules/atoms/read-more/read-more.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export default class ReadMore {
+  constructor() {
+    this.name = 'read-more';
+    console.log('%s module', this.name.toLowerCase());
+  }
+}
---
+++ src/_modules/atoms/read-more/tests/read-more.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import ReadMore from '../read-more';
+
+describe('ReadMore View', function() {
+
+  beforeEach(() => {
+    this.readMore = new ReadMore();
+  });
+
+  it('Should run a few assertions', () => {
+    expect(this.readMore);
+  });
+
+});
